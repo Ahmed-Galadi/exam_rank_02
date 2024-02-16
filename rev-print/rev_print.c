e5d90e9ac1a7a969964010a8d1e15ef6c28178b0
Ahmed-Galadi <galadiahmed99@gmail.com>
2024-02-16 05:44:43 +0100

    my solution for rev_print

---
+++ rev-print/rev_print.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 05:01:20 by agaladi           #+#    #+#             */
+/*   Updated: 2024/02/16 05:42:44 by agaladi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t	ft_strlen(char *str)
+{
+	size_t	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+char	*rev_str(char *str)
+{
+	char	*output;
+	char	tmp;
+	int		length;
+	int		i;
+
+	length = ft_strlen(str);
+	output = (char *)malloc((length * sizeof(char)) + 1);
+	if (!output || 0 == length)
+		return (NULL);
+	i = 0;
+	while (length)
+		output[i++] = str[length-- - 1];
+	output[i] = '\0';
+	return (output);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (2 != argc || 0 == ft_strlen(argv[1]))
+		write(1, "\n", 1);
+	else
+		ft_putstr(rev_str(argv[1]));
+		
+	return (0);
+}
